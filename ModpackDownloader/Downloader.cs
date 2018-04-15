152d1240adea99b60ad185d266c1c1ea61b1d612
Adriana <adriana.smijakova@gmail.com>
2018-04-15 23:03:40 +0200

    Base functionality for Downloader

---
+++ ModpackDownloader/Downloader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ModpackDownloader
+{
+    class Downloader : IDisposable
+    {
+        private HttpClient Client
+        {
+            get;
+
+            set;
+        }
+
+
+        public Downloader()
+        {
+            Client = new HttpClient();
+        }
+
+
+        async Task<Stream> GetFileAsStream(string fileUri)
+        {
+            if (string.IsNullOrEmpty(fileUri))
+            {
+                throw new ArgumentNullException(nameof(fileUri));
+            }
+
+            var result = await Client.GetAsync(fileUri);
+            if (!result.IsSuccessStatusCode)
+            {
+                // TODO add logging
+                return null;
+            }
+
+            return await result.Content.ReadAsStreamAsync();
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (Client != null)
+                {
+                    Client.Dispose();
+                }
+            }
+        }
+    }
+}
